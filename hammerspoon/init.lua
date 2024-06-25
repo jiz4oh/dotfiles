8e8c844c5516da020b6dc7178e5c7f4177c9fbac
jiz4oh <me@jiz4oh.com>
2024-06-25 14:20:02 +0800

    Allow set different terminal

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,22 +1,32 @@
 -- https://github.com/kovidgoyal/kitty/issues/45#issuecomment-573196169
-hs.hotkey.bind({"cmd"}, "F12", function()
-  -- osascript -e 'id of app "kitty"'
-  local app_bundle_id = "net.kovidgoyal.kitty"
-  local app_bundle_id = "com.github.wez.wezterm"
-  local app = hs.application.get(app_bundle_id)
+hs.hotkey.bind({ "cmd" }, "F12", function()
+	-- osascript -e 'id of app "kitty"'
+  local terminal = os.getenv("TERMINAL")
+  local app_bundle_id
 
-  if app then
-      if not app:mainWindow() then
-          app:selectMenuItem({"kitty", "New OS window"})
-      elseif app:isFrontmost() then
-          app:hide()
-      else
-          app:activate()
-      end
+  if terminal == "kitty" then
+    app_bundle_id = "net.kovidgoyal.kitty"
+  elseif terminal == "wezterm" then
+    app_bundle_id = "com.github.wez.wezterm"
   else
-      hs.application.launchOrFocusByBundleID(app_bundle_id)
-      app = hs.application.get(app_bundle_id)
+    app_bundle_id = "net.kovidgoyal.kitty"
   end
 
-  app:mainWindow():moveToUnit'[100,100,0,0]'
+	local app = hs.application.get(app_bundle_id)
+
+	if app then
+		if app:isFrontmost() then
+			app:hide()
+		else
+			app:activate()
+			-- if not app:mainWindow() then
+			-- 	app:selectMenuItem({ "kitty", "New OS window" })
+			-- end
+		end
+	else
+		hs.application.launchOrFocusByBundleID(app_bundle_id)
+		app = hs.application.get(app_bundle_id)
+	end
+
+	app:mainWindow():moveToUnit("[100,100,0,0]")
 end)
