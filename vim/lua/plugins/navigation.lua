5cc668f852153627b66ca78b043ac6267d69da25
jiz4oh <me@jiz4oh.com>
2024-11-30 21:54:13 +0800

    feat(plugins): add netman.nvim for remote filesystem access
    Integrates netman.nvim plugin with lazy loading configuration to enable
    remote filesystem operations through commands like NmloadProvider, Nmlogs,
    Nmdelete, Nmread, and Nmwrite.

--- vim/lua/plugins/navigation.lua
+++ vim/lua/plugins/navigation.lua
@@ -7,6 +7,19 @@ local plugin = {
 			{ "PhilRunninger/nerdtree-visual-selection" },
 		},
 	},
+	{
+		"miversen33/netman.nvim",
+		event = "CmdlineEnter",
+		cmd = {
+      "NmloadProvider",
+      "Nmlogs",
+      "Nmdelete",
+      "Nmread",
+      "Nmwrite",
+		},
+		lazy = not (vim.fn.argc() > 0),
+		opts = {},
+	},
 }
 
 return plugin
