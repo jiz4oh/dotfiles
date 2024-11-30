8907d8e343836f9a9dfdd67daa0248a97bd4247f
jiz4oh <me@jiz4oh.com>
2024-11-30 22:33:13 +0800

    refactor: migrate treesitter move to snacks.nvim
    Remove treesitter-based movement keymaps ([], ][, etc) and add snacks.nvim for
    better word navigation and utility toggles. This change provides more
    consistent jump behavior and adds additional quality-of-life toggles for
    various Neovim settings.

--- vim/lua/plugins/treesitter.lua
+++ vim/lua/plugins/treesitter.lua
@@ -65,26 +65,6 @@ local plugin = {
 					["am"] = "@call.outer",
 					["im"] = "@call.inner",
 				},
-				move = {
-					enable = true,
-					set_jumps = true, -- whether to set jumps in the jumplist
-					goto_next_start = {
-						["]m"] = "@function.outer",
-						["]]"] = "@class.outer",
-					},
-					goto_next_end = {
-						["]M"] = "@function.outer",
-						["]["] = "@class.outer",
-					},
-					goto_previous_start = {
-						["[m"] = "@function.outer",
-						["[["] = "@class.outer",
-					},
-					goto_previous_end = {
-						["[M"] = "@function.outer",
-						["[]"] = "@class.outer",
-					},
-				},
 			},
 		},
 		config = function(_, opts)
