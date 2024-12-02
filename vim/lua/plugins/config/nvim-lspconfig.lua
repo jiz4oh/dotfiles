46ba5a81d447d083b7540011cb12f73e6e5f137c
jiz4oh <me@jiz4oh.com>
2024-12-02 11:56:14 +0800

    fix(nvim): adjust lsp loading to trigger on buffer events
    Change the LSP initialization event from "VeryLazy" to "BufReadPre" and
    "BufNewFile" events, following LazyVim's implementation for better LSP
    loading behavior.

--- vim/lua/plugins/config/nvim-lspconfig.lua
+++ vim/lua/plugins/config/nvim-lspconfig.lua
@@ -1,7 +1,8 @@
 return {
 	"neovim/nvim-lspconfig",
 	enabled = vim.g.as_ide == 1,
-	event = "VeryLazy",
+	-- https://github.com/LazyVim/LazyVim/blob/86ac9989ea15b7a69bb2bdf719a9a809db5ce526/lua/lazyvim/plugins/lsp/init.lua#L5
+	event = { "BufReadPre", "BufNewFile" },
 	keys = {
 		{
 			"<leader>lr",
