2c6246e272e1245d27e767a754fb1b331f8a516e
jiz4oh <me@jiz4oh.com>
2024-12-05 17:11:58 +0800

    feat(mason-lspconfig): refactor LSP server configurations
    Refactored the LSP server configurations for lua_ls, jsonls, and yamlls
    to use a more modular approach. Moved the configurations to a separate
    table and updated the handlers to use this new structure. This change
    improves code readability and maintainability.

--- vim/lua/plugins/config/mason-lspconfig.nvim.lua
+++ vim/lua/plugins/config/mason-lspconfig.nvim.lua
@@ -1,3 +1,51 @@
+local configs = {
+	["lua_ls"] = {
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim" },
+				},
+			},
+		},
+	},
+	["jsonls"] = function()
+		local opts = {}
+		local ok1, schemasstore = pcall(require, "schemastore")
+		if ok1 then
+			opts = {
+				settings = {
+					json = {
+						schemas = schemasstore.json.schemas(),
+						validate = { enable = true },
+					},
+				},
+			}
+		end
+		return opts
+	end,
+	["yamlls"] = function()
+		local opts = {}
+		local ok1, schemasstore = pcall(require, "schemastore")
+		if ok1 then
+			opts = {
+				settings = {
+					yaml = {
+						schemaStore = {
+							-- You must disable built-in schemaStore support if you want to use
+							-- this plugin and its advanced options like `ignore`.
+							enable = false,
+							-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+							url = "",
+						},
+						schemas = schemasstore.yaml.schemas(),
+					},
+				},
+			}
+		end
+		return opts
+	end,
+}
+
 return {
 	"williamboman/mason-lspconfig.nvim",
 	dependencies = "williamboman/mason.nvim",
@@ -28,7 +76,7 @@ return {
 		end
 
 		local self_managed_servers = { "tsserver", "ts_ls", "rubocop" }
-		return {
+		local res = {
 			handlers = {
 				-- The first entry (without a key) will be the default handler
 				-- and will be called for each installed server that doesn't have
@@ -43,58 +91,21 @@ return {
 					local opts = vim.tbl_deep_extend("keep", {}, default_opts())
 					require("lspconfig")[server_name].setup(opts)
 				end,
-				["lua_ls"] = function()
-					local opts = vim.tbl_deep_extend("keep", {
-						settings = {
-							Lua = {
-								diagnostics = {
-									globals = { "vim" },
-								},
-							},
-						},
-					}, default_opts())
-
-					require("lspconfig")["lua_ls"].setup(opts)
-				end,
-				["jsonls"] = function()
-					local opts = {}
-					local ok1, schemasstore = pcall(require, "schemastore")
-					if ok1 then
-						opts = {
-							settings = {
-								json = {
-									schemas = schemasstore.json.schemas(),
-									validate = { enable = true },
-								},
-							},
-						}
-					end
-
-					require("lspconfig")["jsonls"].setup(vim.tbl_deep_extend("keep", opts, default_opts()))
-				end,
-				["yamlls"] = function()
-					local opts = {}
-					local ok1, schemasstore = pcall(require, "schemastore")
-					if ok1 then
-						opts = {
-							settings = {
-								yaml = {
-									schemaStore = {
-										-- You must disable built-in schemaStore support if you want to use
-										-- this plugin and its advanced options like `ignore`.
-										enable = false,
-										-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
-										url = "",
-									},
-									schemas = schemasstore.yaml.schemas(),
-								},
-							},
-						}
-					end
-
-					require("lspconfig")["yamlls"].setup(vim.tbl_deep_extend("keep", opts, default_opts()))
-				end,
 			},
 		}
+
+		for k, v in pairs(configs) do
+			res.handlers[k] = function()
+				local o = v
+				if type(v) == "function" then
+					o = v()
+				end
+				local opts = vim.tbl_deep_extend("keep", o, default_opts())
+
+				require("lspconfig")[k].setup(opts)
+			end
+		end
+
+		return res
 	end,
 }
