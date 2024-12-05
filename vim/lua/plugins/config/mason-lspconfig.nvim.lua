9390c2fc11029e8c63843aa80fbbffdba4024c54
jiz4oh <me@jiz4oh.com>
2024-12-05 17:17:08 +0800

    feat(mason-lspconfig): add harper_ls configuration
    Added configuration for harper_ls in mason-lspconfig. This includes
    settings for linters such as disabling spell check and sentence
    capitalization.

--- vim/lua/plugins/config/mason-lspconfig.nvim.lua
+++ vim/lua/plugins/config/mason-lspconfig.nvim.lua
@@ -1,4 +1,16 @@
 local configs = {
+	["harper_ls"] = {
+		settings = {
+			["harper-ls"] = {
+				linters = {
+					-- Use typos for spell checking
+					spell_check = false,
+					-- do not care capitalization
+					sentence_capitalization = false,
+				}
+			}
+		},
+	},
 	["lua_ls"] = {
 		settings = {
 			Lua = {
