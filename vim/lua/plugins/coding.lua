f00a139195388eb5fce67f21199549e6854aedaf
jiz4oh <me@jiz4oh.com>
2024-12-13 18:17:14 +0800

    refactor(plugins): move nvim-cmp to extras
    Move nvim-cmp and related plugins to extras/coding/nvim-cmp.lua for better
    organization. This change follows lazy.nvim's pattern of organizing optional
    plugins into extras folder for cleaner core config.

--- vim/lua/plugins/coding.lua
+++ vim/lua/plugins/coding.lua
@@ -7,43 +7,7 @@ return {
 		import = "plugins.config.LuaSnip"
 	},
 	{
-		"zbirenbaum/copilot.lua",
 		enabled = enabled,
-		import = "plugins.config.copilot"
-	},
-	{
-		"zbirenbaum/copilot-cmp",
-		enabled = enabled,
-		import = "plugins.config.copilot-cmp"
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		enabled = enabled,
-		import = "plugins.config.nvim-cmp"
-	},
-	{
-		import = "plugins.config.cmp-buffer"
-	},
-	{
-		import = "plugins.config.cmp-cmdline"
-	},
-	{
-		import = "plugins.config.cmp-nvim-tags"
-	},
-	{
-		import = "plugins.config.cmp-path"
-	},
-	{
-		import = "plugins.config.cmp_luasnip"
-	},
-	{
-		"saadparwaiz1/cmp_luasnip",
-		enabled = enabled,
-		import = "plugins.config.cmp_luasnip"
-	},
-	{
-		"garyhurtz/cmp_kitty",
-		enabled = enabled,
-		import = "plugins.config.cmp_kitty"
+		import = "plugins.extras.coding.nvim-cmp"
 	},
 }
---
+++ vim/lua/plugins/extras/coding/nvim-cmp.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		import = "plugins.config.copilot"
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		import = "plugins.config.copilot-cmp"
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		import = "plugins.config.nvim-cmp"
+	},
+	{
+		import = "plugins.config.cmp-buffer"
+	},
+	{
+		import = "plugins.config.cmp-cmdline"
+	},
+	{
+		import = "plugins.config.cmp-nvim-tags"
+	},
+	{
+		import = "plugins.config.cmp-path"
+	},
+	{
+		import = "plugins.config.cmp_luasnip"
+	},
+	{
+		"saadparwaiz1/cmp_luasnip",
+		import = "plugins.config.cmp_luasnip"
+	},
+	{
+		"garyhurtz/cmp_kitty",
+		import = "plugins.config.cmp_kitty"
+	},
+	{
+		import = "plugins.config.cmp_luasnip"
+	}
+}
