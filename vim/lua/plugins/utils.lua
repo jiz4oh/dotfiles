51c7f28b838c5cfd91725abd0b426ba4de4b3a16
jiz4oh <me@jiz4oh.com>
2025-02-25 19:34:19 +0800

    fix(plugin): nvim-treesitter ininitlize failed

--- vim/lua/plugins/utils.lua
+++ vim/lua/plugins/utils.lua
@@ -1,24 +1,27 @@
 return {
 	{
-		import = "plugins.config.firenvim"
+		import = "plugins.config.nvim-treesitter",
+	},
+	{
+		import = "plugins.config.firenvim",
 	},
 	{
 		"folke/which-key.nvim",
 		enabled = vim.fn.has("nvim-0.10") == 1,
-		import = "plugins.config.which-key"
+		import = "plugins.config.which-key",
 	},
 	{
 		"williamboman/mason.nvim",
-		import = "plugins.config.mason"
+		import = "plugins.config.mason",
 	},
 	{
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		import = "plugins.config.mason-tool-installer"
+		import = "plugins.config.mason-tool-installer",
 	},
 	{
 		"folke/snacks.nvim",
 		enabled = vim.fn.has("nvim-0.9.4") == 1,
-		import = "plugins.config.snacks"
+		import = "plugins.config.snacks",
 	},
 	{
 		"mhinz/vim-hugefile",
