182000563f736c13db393492d092b59090b0e652
jiz4oh <me@jiz4oh.com>
2025-03-12 14:34:32 +0800

    refactor(hammerspoon): move to spoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,41 +1,12 @@
 hs.ipc.cliInstall()
 
--- https://github.com/kovidgoyal/kitty/issues/45#issuecomment-573196169
-hs.hotkey.bind({ "cmd" }, "F12", function()
-	-- osascript -e 'id of app "kitty"'
-	local terminal = os.getenv("TERMINAL")
-	local app_bundle_id
-	if terminal == "kitty" then
-		app_bundle_id = "net.kovidgoyal.kitty"
-	elseif terminal == "wezterm" then
-		app_bundle_id = "com.github.wez.wezterm"
-	else
-		app_bundle_id = "net.kovidgoyal.kitty"
-	end
-
-	local app = hs.application.get(app_bundle_id)
-
-	if app then
-		if app:isFrontmost() then
-			app:hide()
-		else
-			app:activate()
-			-- if not app:mainWindow() then
-			-- 	app:selectMenuItem({ "kitty", "New OS window" })
-			-- end
-		end
-	else
-		hs.application.launchOrFocusByBundleID(app_bundle_id)
-		app = hs.application.get(app_bundle_id)
-	end
-
-	app:mainWindow():moveToUnit("[100,100,0,0]")
-end)
-
 hs.loadSpoon("SpoonInstall")
 spoon.SpoonInstall:andUse("EmmyLua")
 spoon.SpoonInstall:andUse("ReloadConfiguration", { start = true })
 
 hs.spoons.use("ime", {
+	hotkeys = "default",
+})
+hs.spoons.use("window", {
 	hotkeys = "default",
 })
