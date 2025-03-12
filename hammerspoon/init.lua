8750ec50376b6f04d86540e1c0fbc1c125b21327
jiz4oh <me@jiz4oh.com>
2025-03-12 14:29:56 +0800

    feat(hammerspoon): enable ipc by default

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,16 +1,17 @@
+hs.ipc.cliInstall()
+
 -- https://github.com/kovidgoyal/kitty/issues/45#issuecomment-573196169
 hs.hotkey.bind({ "cmd" }, "F12", function()
 	-- osascript -e 'id of app "kitty"'
-  local terminal = os.getenv("TERMINAL")
-  local app_bundle_id
-
-  if terminal == "kitty" then
-    app_bundle_id = "net.kovidgoyal.kitty"
-  elseif terminal == "wezterm" then
-    app_bundle_id = "com.github.wez.wezterm"
-  else
-    app_bundle_id = "net.kovidgoyal.kitty"
-  end
+	local terminal = os.getenv("TERMINAL")
+	local app_bundle_id
+	if terminal == "kitty" then
+		app_bundle_id = "net.kovidgoyal.kitty"
+	elseif terminal == "wezterm" then
+		app_bundle_id = "com.github.wez.wezterm"
+	else
+		app_bundle_id = "net.kovidgoyal.kitty"
+	end
 
 	local app = hs.application.get(app_bundle_id)
 
