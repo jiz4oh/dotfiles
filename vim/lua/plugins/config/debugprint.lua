ad28a749815624af9797faf22e4ed776477bb549
jiz4oh <me@jiz4oh.com>
2025-03-14 15:49:19 +0800

    feat(nvim): configure debugprint.nvim keymaps
    Add keyboard mappings and commands configuration for debugprint.nvim:
    - Set normal mode mappings (g?p, g?P, g?v, g?V, g?o, g?O)
    - Configure insert mode shortcuts (ctrl-g + p/v)
    - Add visual mode mappings
    - Define plugin commands for managing debug prints

--- vim/lua/plugins/config/debugprint.lua
+++ vim/lua/plugins/config/debugprint.lua
@@ -1,3 +1,68 @@
 return {
 	"andrewferrier/debugprint.nvim",
+	keys = {
+		"g?p",
+		"g?P",
+		{
+			"g?v",
+			mode = { "n", "v" },
+		},
+		{
+			"g?V",
+			mode = { "n", "v" },
+		},
+		{
+			"ctrl-g",
+			"p",
+			mode = "i",
+		},
+		{
+			"ctrl-g",
+			"v",
+			mode = "i",
+		},
+		{
+			"g?o",
+			mode = { "o" },
+		},
+		{
+			"g?O",
+			mode = { "o" },
+		},
+	},
+	cmd = {
+		"ToggleCommentDebugPrints",
+		"DeleteDebugPrints",
+		"ResetDebugPrintsCounter",
+	},
+	opts = {
+		keymaps = {
+			normal = {
+				plain_below = "g?p",
+				plain_above = "g?P",
+				variable_below = "g?v",
+				variable_above = "g?V",
+				variable_below_alwaysprompt = "",
+				variable_above_alwaysprompt = "",
+				textobj_below = "g?o",
+				textobj_above = "g?O",
+				toggle_comment_debug_prints = "",
+				delete_debug_prints = "",
+			},
+			insert = {
+				plain = "<C-G>p",
+				variable = "<C-G>v",
+			},
+			visual = {
+				variable_below = "g?v",
+				variable_above = "g?V",
+			},
+		},
+		commands = {
+			toggle_comment_debug_prints = "ToggleCommentDebugPrints",
+			delete_debug_prints = "DeleteDebugPrints",
+			reset_debug_prints_counter = "ResetDebugPrintsCounter",
+		},
+		-- … Other options
+	},
 }
