6754680cde5aca599e27bbdbed94ec22f116111d
jiz4oh <me@jiz4oh.com>
2025-03-14 15:21:34 +0800

    feat: add keymaps to copy diagnostics to clipboard
    Adds two keymaps for copying diagnostics from current line to clipboard:
    - <leader>yd for nvim 0.10+ using OSC52 clipboard
    - yd for older nvim versions using setreg

--- vim/config/base.lua
+++ vim/config/base.lua
@@ -1,3 +1,13 @@
+local function get_diagnostics()
+	local line = vim.fn.line(".") - 1
+	local diagnostics = vim.diagnostic.get(0, { lnum = line })
+	if #diagnostics > 0 then
+		return table.concat(vim.tbl_map(function(d)
+			return d.message
+		end, diagnostics), " ")
+	end
+end
+
 -- https://github.com/neovim/neovim/pull/25872
 -- https://github.com/neovim/neovim/pull/26064
 -- :h clipboard-osc52
@@ -17,6 +27,16 @@ if vim.fn.has("nvim-0.10") == 1 then
 				end
 			end,
 		})
+
+		vim.keymap.set("n", "<leader>yd", function()
+			local diagnostics = get_diagnostics()
+			if diagnostics ~= nil then
+        require("vim.ui.clipboard.osc52").copy("+")({ diagnostics })
+				vim.notify("Diagnostic copied to clipboard", vim.log.levels.INFO)
+			else
+				vim.notify("No diagnostics on current line", vim.log.levels.WARN)
+			end
+		end)
 	end
 end
 
@@ -38,4 +58,14 @@ if vim.fn.has("nvim-0.6") == 1 then
 
 	vim.diagnostic.config({ float = { severity_sort = true, source = true } })
 	vim.diagnostic.config({ virtual_text = { severity_sort = true, source = true, format = my_diagnostic_format_func } })
+
+	vim.keymap.set("n", "yd", function()
+		local diagnostics = get_diagnostics()
+		if #diagnostics ~= nil then
+			vim.fn.setreg("+", diagnostics)
+			vim.notify("Diagnostic copied to clipboard", vim.log.levels.INFO)
+		else
+			vim.notify("No diagnostics on current line", vim.log.levels.WARN)
+		end
+	end)
 end
