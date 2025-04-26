cf7980024f7bb0ae7568978d0a67a5764a4713c1
jiz4oh <me@jiz4oh.com>
2025-04-26 11:29:40 +0800

    feat(vim): replace netman with oil.nvim
    Replace Netman file explorers with Oil.nvim for a simpler and
    more integrated file navigation experience

---
+++ vim/lua/plugins/config/oil.lua
@@ -0,0 +1,63 @@
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
+---@type LazyPluginSpec
+return {
+  "stevearc/oil.nvim",
+  optional = true,
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory", mode = "n" },
+    { "_", "<CMD>execute 'Oil ' . getcwd()<CR>", desc = "Open parent directory", mode = "n" },
+  },
+  event = {
+    "BufEnter",
+    "BufNew",
+    "FileType",
+  },
+  -- specs = {
+  --   { "preservim/nerdtree", enabled = false },
+  -- },
+  lazy = false,
+  ---@type oil.setupOpts
+  opts = {
+    delete_to_trash = true,
+    win_options = {
+      winbar = "%!v:lua.get_oil_winbar()",
+    },
+    keymaps = {
+      ["K"] = {
+        desc = "Toggle file detail view",
+        callback = function()
+          detail = not detail
+          if detail then
+            require("oil").set_columns({ "icon", "permissions", "size", "mtime" })
+          else
+            require("oil").set_columns({ "icon" })
+          end
+        end,
+      },
+      ["<backspace>"] = { "actions.parent", mode = "n" },
+      ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+      ["?"] = { "actions.show_help", mode = "n" },
+      ["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+      ["<C-g>"] = { "actions.yank_entry", nowait = true },
+      ["<leader>:"] = {
+        "actions.open_cmdline",
+        opts = {
+          shorten_path = true,
+          modify = ":h",
+        },
+        desc = "Open the command line with the current directory as an argument",
+      },
+    },
+  },
+}
--- vim/lua/plugins/navigation.lua
+++ vim/lua/plugins/navigation.lua
@@ -1,12 +1,26 @@
 ---@type LazySpec[]
 return {
+  { "stevearc/oil.nvim" },
+  {
+    -- "refractalize/oil-git-status.nvim", -- it doesn't display unstaged git status
+    "SirZenith/oil-vcs-status",
+    dependencies = {
+      "stevearc/oil.nvim",
+      opts = {
+        win_options = {
+          signcolumn = "yes:2",
+        },
+      },
+    },
+    config = true,
+  },
   {
     "preservim/nerdtree",
     import = "plugins.config.nerdtree",
   },
   {
-    "miversen33/netman.nvim",
-    import = "plugins.config.netman",
+    -- "miversen33/netman.nvim",
+    -- import = "plugins.config.netman",
   },
   {
     "hedyhli/outline.nvim",
