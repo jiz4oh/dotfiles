952f80f2094531649fce49bda41877ceb1bfa03c
jiz4oh <me@jiz4oh.com>
2025-05-14 11:26:52 +0800

    feat(overseer): expand plugin commands and keymaps

--- vim/lua/plugins/config/overseer.lua
+++ vim/lua/plugins/config/overseer.lua
@@ -8,12 +8,29 @@ return {
     { "<leader>ol", "<cmd>OverseerRestartLast<cr>", desc = "Restart last task" },
     { "<leader>of", "<cmd>OverseerFloatLast<cr>", desc = "Open last task in float window" },
     { "<leader>oa", "<cmd>OverseerTaskAction<cr>", desc = "Select a task to run an action on" },
-    { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Run an action on the most recent task, or the task under the cursor" },
+    {
+      "<leader>oq",
+      "<cmd>OverseerQuickAction<cr>",
+      desc = "Run an action on the most recent task, or the task under the cursor",
+    },
+    { "<leader>oi", "<cmd>OverseerInfo<cr>", desc = "Overseer Info" },
+    { "<leader>ob", "<cmd>OverseerBuild<cr>", desc = "Task builder" },
+    { "<leader>oc", "<cmd>OverseerClearCache<cr>", desc = "Clear cache" },
   },
   cmd = {
+    "OverseerOpen",
+    "OverseerClose",
     "OverseerToggle",
+    "OverseerSaveBundle",
+    "OverseerLoadBundle",
+    "OverseerDeleteBundle",
+    "OverseerRunCmd",
     "OverseerRun",
-    "OverseerRestartLast",
+    "OverseerInfo",
+    "OverseerBuild",
+    "OverseerQuickAction",
+    "OverseerTaskAction",
+    "OverseerClearCache",
   },
   init = function()
     vim.api.nvim_create_user_command("OverseerRestartLast", function()
@@ -65,4 +82,15 @@ return {
     -- $_DOTFILES_PATH/vim/lua/overseer/template/user.lua
     require("overseer").load_template("user")
   end,
+  specs = {
+    {
+      "folke/which-key.nvim",
+      optional = true,
+      opts = {
+        spec = {
+          { "<leader>o", group = "overseer" },
+        },
+      },
+    },
+  },
 }
