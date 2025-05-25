92168e16e497f0985d055567687771f9916cad34
jiz4oh <me@jiz4oh.com>
2025-05-25 16:23:25 +0800

    feat(docker): improve overseer docker template
    * Add container name suggestions from running containers for better UX
    * Support complex docker commands with space-separated arguments
    * Add predefined list of container-specific commands
    * Fix handling of command arguments array

--- vim/lua/overseer/template/docker.lua
+++ vim/lua/overseer/template/docker.lua
@@ -1,28 +1,71 @@
 local files = require("overseer.files")
 
+local cmds = {
+  "start",
+  "stop",
+  "exec",
+  "restart",
+  "rm",
+  "logs",
+  "inspect",
+  "top",
+  "stats",
+  "attach",
+  -- "cp", -- complicated by the fact that it can be used to copy files
+  "diff",
+  "pause",
+  "unpause",
+  "wait",
+  "port",
+  "rename",
+  "update",
+  "commit",
+}
+
 ---@type overseer.TemplateProvider
 return {
   name = "docker ",
   priority = 60,
-  params = {
-    task = {
-      desc = "Docker command (e.g., build, run, ps, images)",
-      type = "string",
-      default = "ps",
-      order = 1,
-    },
-    extra_params = {
-      type = "string",
-      desc = "Add extra parameter(s) for the docker command",
-      optional = true,
-      order = 2,
-    },
-  },
+  params = function() -- https://github.com/stevearc/overseer.nvim/issues/86
+    local stdout = vim.system({ "docker", "ps", "--format", "{{lower .Names}}" }):wait().stdout
+    local containers = vim.split(stdout or "", "\n", { trimempty = true })
+    return {
+      cmd = {
+        desc = "Docker command (e.g., build, run, ps, images)",
+        type = "list",
+        delimiter = " ",
+        order = 1,
+      },
+      container = {
+        desc = "Which container to use",
+        type = "enum",
+        order = 2,
+        choices = containers,
+        optional = true,
+      },
+      extra_params = {
+        type = "string",
+        desc = "Add extra parameter(s) for the docker command",
+        optional = true,
+        order = 3,
+      },
+    }
+  end,
   builder = function(params)
     local args = {}
 
-    if params.task then
-      table.insert(args, params.task)
+    if params.cmd then
+      local container = false
+      for _, v in ipairs(params.cmd) do
+        table.insert(args, v)
+        if container or vim.tbl_contains(cmds, v) then
+          container = true
+        end
+      end
+
+      if container and params.container then
+        table.insert(args, params.container)
+      end
     end
 
     if params.extra_params then
@@ -34,7 +77,7 @@ return {
 
     ---@type overseer.TaskDefinition
     local task_def = {
-      cmd = "docker",
+      cmd = { "docker" },
       args = args,
       components = {
         "dispatch", -- Use dispatch component like in docker-compose
