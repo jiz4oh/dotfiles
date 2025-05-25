e2a953a4f065dcb0970dc6afe9cdc044172bb311
jiz4oh <me@jiz4oh.com>
2025-05-25 14:31:19 +0800

    refactor(template): improve PDM template structure

--- vim/lua/overseer/template/pdm.lua
+++ vim/lua/overseer/template/pdm.lua
@@ -1,4 +1,39 @@
 local files = require("overseer.files")
+local overseer = require("overseer")
+
+---@type overseer.TemplateFileDefinition
+local tmpl = {
+  cache_key = function(opts)
+    return opts.dir
+  end,
+  params = {
+    args = { type = "list", delimiter = " " },
+    cwd = { optional = true },
+  },
+  builder = function(params)
+    local args = params.args or {}
+    return {
+      cmd = "pdm",
+      args = args,
+      cwd = params.cwd,
+      components = {
+        -- { "on_output_quickfix", set_diagnostics = true },
+        -- "on_result_diagnostics",
+        -- https://github.com/stevearc/overseer.nvim/blob/master/doc/components.md#open_output
+        {
+          "open_output",
+          on_start = "if_no_on_output_quickfix",
+          on_complete = "failure",
+        },
+        -- https://github.com/stevearc/overseer.nvim/blob/master/doc/components.md#unique
+        {
+          "unique",
+        },
+        "default",
+      },
+    }
+  end,
+}
 
 ---@type overseer.TemplateProvider
 return {
@@ -11,80 +46,42 @@ return {
   },
   generator = function(opts, cb)
     local ret = {
-      {
+      overseer.wrap_template(tmpl, {
         name = "(PDM) install ",
-        cache_key = function(opts)
-          return opts.dir
-        end,
         condition = {
           callback = function(opts)
-            return files.exists(files.join(opts.dir, "pdm.lock") or files.exists(files.join(opts.dir, "pyproject.toml")))
+            return files.exists(
+              files.join(opts.dir, "pdm.lock")
+                or files.exists(files.join(opts.dir, "pyproject.toml"))
+            )
           end,
         },
-        builder = function()
-          return {
-            cmd = "pdm",
-            args = {
-              "install",
-              "-p",
-              opts.dir
-            },
-            components = {
-              -- { "on_output_quickfix", set_diagnostics = true },
-              -- "on_result_diagnostics",
-              -- https://github.com/stevearc/overseer.nvim/blob/master/doc/components.md#open_output
-              {
-                "open_output",
-                on_start = "if_no_on_output_quickfix",
-                on_complete = "failure",
-              },
-              -- https://github.com/stevearc/overseer.nvim/blob/master/doc/components.md#unique
-              {
-                "unique",
-              },
-              "default",
-            },
-          }
-        end,
-      },
+      }, {
+        args = {
+          "install",
+          "-p",
+          opts.dir,
+        },
+      }),
     }
     for _, filename in ipairs({ "requirements.txt", "setup.py" }) do
-      table.insert(ret, {
-        name = "(PDM) import " .. filename,
-        cache_key = function(opts)
-          return files.join(opts.dir, filename)
-        end,
-        condition = {
-          callback = function(opts)
-            return files.exists(files.join(opts.dir, filename))
-              and not files.exists(files.join(opts.dir, "/pyproject.toml"))
-          end,
-        },
-        builder = function()
-          return {
-            cmd = "pdm",
-            args = {
-              "import",
-              filename,
-            },
-            components = {
-              -- { "on_output_quickfix", set_diagnostics = true },
-              -- "on_result_diagnostics",
-              -- https://github.com/stevearc/overseer.nvim/blob/master/doc/components.md#open_output
-              {
-                "open_output",
-                on_start = "if_no_on_output_quickfix",
-                on_complete = "failure",
-              },
-              -- https://github.com/stevearc/overseer.nvim/blob/master/doc/components.md#unique
-              {
-                "unique",
-              },
-              "default",
-            },
-          }
-        end,
-      })
+      table.insert(
+        ret,
+        overseer.wrap_template(tmpl, {
+          name = "(PDM) import " .. filename,
+          condition = {
+            callback = function(opts)
+              return files.exists(files.join(opts.dir, filename))
+                and not files.exists(files.join(opts.dir, "/pyproject.toml"))
+            end,
+          },
+        }, {
+          args = {
+            "import",
+            filename,
+          },
+        })
+      )
     end
 
     cb(ret)
