b2b0039706fb8d55b1a846730b37b62fd7a8414b
jiz4oh <me@jiz4oh.com>
2025-09-19 09:07:40 +0800

    feat(snacks): handle file renames from Oil actions
    Add autocmd to listen for Oil move actions and trigger Snacks rename handler
    to maintain proper references when files are renamed through Oil interface.

--- vim/lua/plugins/config/snacks.lua
+++ vim/lua/plugins/config/snacks.lua
@@ -226,6 +226,15 @@ return {
           return
         end
 
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "OilActionsPost",
+          callback = function(event)
+              if event.data.actions.type == "move" then
+                  Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+              end
+          end,
+        })
+
         -- enable numbers since has bigfile plugin
         vim.o.number = true
 
