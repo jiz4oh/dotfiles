63c261417502049bec9dc0fcb8ce2f8d7781aade
jiz4oh <me@jiz4oh.com>
2025-11-17 15:30:51 +0800

    feat(lsp/lua_ls): exclude unused variables matching _*
    Configure lua_ls to ignore unused local variables that match the
    pattern "_*", following Lua conventions where underscore-prefixed
    names indicate intentionally unused values.

--- vim/lsp/lua_ls.lua
+++ vim/lsp/lua_ls.lua
@@ -3,6 +3,10 @@ return {
     Lua = {
       diagnostics = {
         globals = { "vim" },
+        -- https://github.com/LuaLS/vscode-lua/blob/fb6694c6aab4ecbceeff1b23d8d5d9aee268ccdd/setting/schema.json#L2507-L2515
+        unusedLocalExclude = {
+          "_*",
+        },
       },
       hint = {
         enable = true,
