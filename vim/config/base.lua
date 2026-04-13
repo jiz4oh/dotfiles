40d9a42158c009b59c06617a4da40a0fbb9a5b8d
jiz4oh <git@jiz4oh.com>
2026-04-13 17:19:09 +0800

    feat: add error logger

--- vim/config/base.lua
+++ vim/config/base.lua
@@ -1,4 +1,5 @@
 require("config.opts")
+require("config.error_logger")
 require("config.clipboard")
 require("config.diagnostics")
 require("config.lsp")
---
+++ vim/lua/config/error_logger.lua
@@ -0,0 +1,58 @@
+local logfile = vim.fn.stdpath("state") .. "/errors.log"
+local orig_schedule = vim.schedule
+local orig_schedule_wrap = vim.schedule_wrap
+local unpack_fn = unpack or table.unpack
+
+vim.g.async_error_logfile = logfile
+
+local function append_log(message)
+  local sanitized = tostring(message):gsub("%z", "\\0")
+  vim.fn.writefile({
+    ("[%s] %s"):format(os.date("%Y-%m-%d %H:%M:%S"), sanitized),
+    "",
+  }, logfile, "a")
+end
+
+local function format_trace(context, err)
+  return ("%s\n%s"):format(context, debug.traceback(err, 2))
+end
+
+local function handle_async_error(context, err)
+  local trace = format_trace(context, err)
+  append_log(trace)
+  orig_schedule(function()
+    vim.notify(trace, vim.log.levels.ERROR)
+  end)
+  return trace
+end
+
+vim.schedule = function(callback)
+  return orig_schedule(function()
+    local ok, trace = xpcall(callback, function(err)
+      return handle_async_error("vim.schedule callback failed", err)
+    end)
+    if ok then
+      return
+    end
+    return trace
+  end)
+end
+
+vim.schedule_wrap = function(callback)
+  return orig_schedule_wrap(function(...)
+    local args = { ... }
+    local ok, trace = xpcall(function()
+      callback(unpack_fn(args))
+    end, function(err)
+      return handle_async_error("vim.schedule_wrap callback failed", err)
+    end)
+    if ok then
+      return
+    end
+    return trace
+  end)
+end
+
+vim.api.nvim_create_user_command("ErrorLog", function()
+  vim.cmd.edit(vim.g.async_error_logfile)
+end, { desc = "Open async error log" })
