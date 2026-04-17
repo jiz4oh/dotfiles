52f7d885127c6098f8bc2ad4cb3b1d6c80b52d74
jiz4oh <git@jiz4oh.com>
2026-04-17 10:36:56 +0800

    feat(vim): replace data-viewer.nvim by csvview.nvim

--- vim/lua/plugins/editor.lua
+++ vim/lua/plugins/editor.lua
@@ -4,10 +4,14 @@ return {
     "andymass/vim-matchup",
   },
   {
-    "VidocqH/data-viewer.nvim",
-    enabled = vim.fn.has("nvim-0.8") == 1,
-    import = "plugins.config.data-viewer",
+    "hat0uma/csvview.nvim",
   },
+  -- 已经没维护了，大 csv 打开无法翻页
+  -- {
+  --   "VidocqH/data-viewer.nvim",
+  --   enabled = vim.fn.has("nvim-0.8") == 1,
+  --   import = "plugins.config.data-viewer",
+  -- },
   {
     "adriaanzon/vim-textobj-matchit",
     import = "plugins.config.vim-textobj-matchit",
